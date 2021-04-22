67fcacd705caf5e463be32da7574078392d81ac2
harish.d <hairsh.d@dvara.com>
2021-04-22 10:30:32 +0530

    path and port update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import '../config/database';
-import server from '../config/server';
+import './config/database';
+import server from './config/server';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
